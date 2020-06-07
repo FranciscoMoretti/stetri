b51dc3a792d8b2c52f4750cea177e7a5ef648d1d
Francisco Moretti <franciscoemoretti@gmail.com>
2020-06-06 23:43:39 -0300

    Updates

---
+++ precache-manifest.4d648e8189d56b91ba2db715c49cd9eb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "61586b2d8d4bb3a870108503a3add917",
+    "url": "/stetri/index.html"
+  },
+  {
+    "revision": "8c2f8eb79402eafa0eb6",
+    "url": "/stetri/static/css/main.8f8cb1ef.chunk.css"
+  },
+  {
+    "revision": "ec5f2ab19ac7407f1b89",
+    "url": "/stetri/static/js/2.b2fb0c27.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stetri/static/js/2.b2fb0c27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c2f8eb79402eafa0eb6",
+    "url": "/stetri/static/js/main.a7358f46.chunk.js"
+  },
+  {
+    "revision": "7030f53c474e8a118f48",
+    "url": "/stetri/static/js/runtime-main.25755b01.js"
+  },
+  {
+    "revision": "e28975521df42a470a60593872337098",
+    "url": "/stetri/static/media/Pixel-LCD-7.e2897552.woff"
+  },
+  {
+    "revision": "2810fe6b5435e75b3dad63e341b97d02",
+    "url": "/stetri/static/media/bg.2810fe6b.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/stetri/precache-manifest.4d648e8189d56b91ba2db715c49cd9eb.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/stetri/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
